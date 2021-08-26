61e7d33ec3a6188e35c823e205b7c89cd672f6b0
HuzefaUsama25 <huzefausama25@gmail.com>
2021-08-26 19:48:13 +0500

    Final Commit

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-from search_engine_parser import GoogleSearch
+#!/usr/bin/python3
 
-def google(query):
+from re import search
+from search_engine_parser import GoogleSearch
+import sys
+
+
+def get_search_results(query):
     search_args = (query, 1)
     gsearch = GoogleSearch()
     gresults = gsearch.search(*search_args)
-    return gresults['links']
+    return gresults['title']
 
-google('hello')
+
+def words_in_sentence(sentence):
+    return len(sentence.split(" "))
+
+def main():
+    query = sys.argv[1]
+    search_results = list()
+    raw_search_results = get_search_results(query)
+    
+
+    for raw_search_result in raw_search_results:
+        raw_search_result_length = words_in_sentence(raw_search_result)
+
+        if raw_search_result_length < 5:
+            raw_search_results.remove(raw_search_result)
+            
+    print(raw_search_results[0])
+
+
+
+
+if __name__=="__main__":
+    main()
